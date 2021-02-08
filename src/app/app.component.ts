34848ed8627ec8f4f7c89d3c7a20ad2ca092f148
omniamohamed199 <omnia.moha181@gmail.com>
2021-02-08 20:23:27 +0200

    split image into parts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,56 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+declare const CanvasShredder: any;
+declare const Hammer: any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'imagetile';
+  shredder = null;
+  imageUrl;
+  open = false
+  needsUpdate = false;
+  startPosition = {};
+  posX; posY;
+  ngOnInit(): void {
+     this.handleFileSelect()
+  }
+  handleFileSelect() {
+    var dstCanvas = document.getElementById('dst-canvas');
+    var previewArea = document.getElementById('preview-area');
+    var newImage = document.createElement('img');
+    newImage.src="assets/images.png"
+      this.shredder = null;
+      newImage.onload =()=> {
+        this.shredder = new CanvasShredder(newImage, previewArea, { storeOriginalInCanvas: false });
+        var slicePosition = { x: 0, y: 0, size: 120 };
+        var selectPos = document.getElementById('select-position');
+        slicePosition.x = previewArea.offsetWidth / 2;
+        slicePosition.y = previewArea.offsetHeight / 2;
+        selectPos.style.left = slicePosition.x + "px";
+        selectPos.style.top = slicePosition.y + "px";
+        selectPos.style.width = slicePosition.size + "px";
+        selectPos.style.height = slicePosition.size + "px";
+        var mc = new Hammer(previewArea.children[2]);
+        mc.on("tap",  (event) => {
+            this.posX = (event.srcEvent.pageX - previewArea.offsetLeft) - slicePosition.size / 2;
+            this.posY = (event.srcEvent.pageY - previewArea.offsetTop) - slicePosition.size / 2;
+            selectPos.style.left = this.posX + "px";
+            selectPos.style.top = this.posY + "px";
+            selectPos.style.width = slicePosition.size + "px";
+            selectPos.style.height = slicePosition.size + "px";
+            this.imageUrl =  this.shredder.slice(this.posX, this.posY, slicePosition.size, dstCanvas);
+            this.open = true
+          
+        })
+      };
+  }
+  save(event)
+  {
+    console.log(event)
+  }
+ 
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,44 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { Injectable, NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+// particular imports for hammer 
+import * as Hammer from 'hammerjs';
+import {
+  HammerModule, HammerGestureConfig, HAMMER_GESTURE_CONFIG
+}
+  from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { ImageDrawingModule } from 'ngx-image-drawing';
+import { DialogsModule } from '@progress/kendo-angular-dialog';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+
+
+@Injectable()
+export class MyHammerConfig extends HammerGestureConfig {
+  overrides = <any>{
+    swipe: { direction: Hammer.DIRECTION_ALL },
+  };
+}
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HammerModule,
+    AppRoutingModule,
+    ImageDrawingModule,
+    DialogsModule,
+    BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HAMMER_GESTURE_CONFIG,
+      useClass: MyHammerConfig,
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
