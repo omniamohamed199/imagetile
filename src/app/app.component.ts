b0c1d0436f4629f803ca7bfc9d7d1ef43fafd3e6
omniamohamed199 <omnia.moha181@gmail.com>
2021-02-11 19:05:36 +0200

    ereas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit, Renderer2, ViewChild } from '@angular/core';
+import { Component, ElementRef, Input, OnInit, Renderer2, ViewChild } from '@angular/core';
+import { fromEvent } from 'rxjs';
+import { switchMap, takeUntil, pairwise } from 'rxjs/operators'
 import { ImageMapCoordinate } from './imagemap/imagemap.component';
 declare const CanvasShredder: any;
 declare const Hammer: any;
@@ -44,12 +46,11 @@ export class AppComponent implements OnInit {
   //           selectPos.style.height = slicePosition.size + "px";
   //           this.imageUrl =  this.shredder.slice(this.posX, this.posY, slicePosition.size, dstCanvas);
   //           this.open = true
-          
+
   //       })
   //     };
   // }
-  save(event)
-  {
+  save(event) {
     console.log(event)
   }
   image: string = 'https://image.shutterstock.com/image-vector/solar-system-sun-planets-vector-260nw-751091653.jpg'
@@ -85,21 +86,116 @@ export class AppComponent implements OnInit {
   //   // this.open=true
   //   // this.imageUrl='assets/images.png'
   // }
-  constructor (private renderer: Renderer2){}
-  @ViewChild('back', {static: false})back;
-  Whichpart(title)
-  {
-    
-// this.back.mapster({
-//   fillOpacity: 0.9,
-//   fillColor: "ff0000",
-//   stroke: true,
-//   strokeColor: "000000",
-//   strokeWidth: 4,
-//   mapKey: 'name',
-//   showToolTip: true,
-// });
-    console.log(title)
+  constructor(private renderer: Renderer2) { }
+  @ViewChild('canvas', { static: true }) public canvas: ElementRef;
+
+  @Input() public width = 300;
+  @Input() public height = 300;
+  carPart;
+  private cx: CanvasRenderingContext2D;
+  Whichpart(title) {
+    this.carPart = title
+    const canvasEl: HTMLCanvasElement = this.canvas.nativeElement;
+    this.cx = canvasEl.getContext('2d');
+    canvasEl.width = this.width;
+    canvasEl.height = this.height;
+    let newImg = this.renderer.createElement('img');
+    this.cx.lineWidth = 3;
+    this.cx.lineCap = 'round';
+    this.cx.strokeStyle = '#000';
+    this.captureEvents(canvasEl);
+    switch (title) {
+      case 'FrontDoor':
+        newImg.src = "assets/sep-2.png";
+        newImg.onload = this.drawImg(newImg);
+        break;
+      case 'BackDoor':
+        newImg.src = "assets/sep-1.png";
+        newImg.onload = this.drawImg(newImg);
+        break;
+      default:
+        break;
+    }
+
+  }
+  drawImg(imgContext) {
+    console.log('drawImg called');
+    setTimeout(() => {
+      this.cx.drawImage(imgContext, 10, 10, this.width, this.height);
+      this.open = true
+    }, 500);
+
+  }
+  private captureEvents(canvasEl: HTMLCanvasElement) {
+    // this will capture all mousedown events from the canvas element
+    fromEvent(canvasEl, 'touchstart')
+      .pipe(
+        switchMap((e) => {
+          // after a mouse down, we'll record all mouse moves
+          return fromEvent(canvasEl, 'touchmove')
+            .pipe(
+              takeUntil(fromEvent(canvasEl, 'touchend')),
+              // we'll also stop (and unsubscribe) once the mouse leaves the canvas (mouseleave event)
+              takeUntil(fromEvent(canvasEl, 'touchend')),
+              // pairwise lets us get the previous value to draw a line from
+              // the previous point to the current point    
+              pairwise()
+            )
+        })
+      )
+      .subscribe((res: [TouchEvent, TouchEvent]) => {
+        const rect = canvasEl.getBoundingClientRect();
+
+        // previous and current position with the offset
+        const prevPos = {
+          x: res[0].targetTouches[0].clientX - rect.left,
+          y: res[0].targetTouches[0].clientY - rect.top
+        };
+        const currentPos = {
+          x: res[1].targetTouches[0].clientX - rect.left,
+          y: res[1].targetTouches[0].clientY - rect.top
+        };
+        // this method we'll implement soon to do the actual drawing
+        this.drawOnCanvas(prevPos, currentPos);
+      });
+      const touchend = fromEvent(canvasEl, 'touchend')
+      touchend.subscribe(res=>{
+        this.restore_array.push(this.cx.getImageData(0,0,this.width,this.height))
+        this.index+=1
+      })
+  }
+  restore_array = []
+  index = -1;
+  bErasing = false
+  private drawOnCanvas(prevPos: { x: number, y: number }, currentPos: { x: number, y: number }) {
+    if (!this.cx) { return; }
+    this.cx.beginPath();
+    if(this.bErasing == true){
+      this.cx.globalCompositeOperation="destination-out";
+      this.cx.lineWidth = 8;
+    }else{
+      this.cx.globalCompositeOperation="source-over";
+    }
+      this.cx.moveTo(prevPos.x, prevPos.y); // from
+      this.cx.lineTo(currentPos.x, currentPos.y);
+      this.cx.stroke();
+      this.cx.strokeStyle = "#FF0000";
+  }
+  saveIm() {
+    var image = new Image();
+    image.src = this.canvas.nativeElement.toDataURL();
+    console.log(image.src)
+  }
+  undo() {
+    if(this.index<=0)
+    {
+      this.Whichpart(this.carPart)
+    }
+    else{
+      this.index-=1
+      this.restore_array.pop()
+      this.cx.putImageData(this.restore_array[this.index],0,0)
+    }
   }
 }
 
